f2cfbac39d55c1a8ab88dffd325125b48756831d
soapcandy <octi97@naver.com>
2023-07-12 16:58:15 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductInput from './components/ProductInput';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductInput></ProductInput>
+      <ProductList></ProductList>
+    </div>
+  );
+}
+
+export default App;
